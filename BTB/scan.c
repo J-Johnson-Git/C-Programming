42453c8602125f13a912ec047ae8470ad89772be
Josh Johnson <josh.johnson.hire@protonmail.com>
2021-02-18 17:29:00 -0800

    update scan.c
    Cleaned up char buffer sizes, used fgetc() to skip newline chars, cleans up formatting

--- BTB/scan.c
+++ BTB/scan.c
@@ -11,23 +11,30 @@
 
 void scan_trace(FILE *input_file)
 {
-	char current_PC[ADDRESS_LENGTH + 1] = "";  //fgets needs to read \n chars for strcpy to copy correctly
-	char next_PC[ADDRESS_LENGTH + 1] = "";
-
+	char current_PC[ADDRESS_LENGTH] = "";  //fgets needs to read \n chars for strcpy to copy correctly
+	char next_PC[ADDRESS_LENGTH] = "";
+	char skip_newline = 0;
 	int next_PC_index = 0;
 	int current_PC_index = 0;
-	fgets(current_PC, ADDRESS_LENGTH + 1, input_file);  //preload current_PC with first entry
-	while (fgets(next_PC, ADDRESS_LENGTH + 1, input_file))
+
+
+	fgets(current_PC, ADDRESS_LENGTH, input_file);  //preload current_PC with first entry
+	skip_newline = fgetc(input_file);
+	while (fgets(next_PC, ADDRESS_LENGTH, input_file))
 	{
+		//retrieve line, skip newline char, find index numbers
+		skip_newline = fgetc(input_file);
 		current_PC_index = index_calc(current_PC);
 		next_PC_index = index_calc(next_PC);
+
 		if (next_PC_index != (current_PC_index + 1))  //branch condition, adding to BTB
 		{
 			BTB_array[current_PC_index].entry = current_PC_index;
 			strcpy(BTB_array[current_PC_index].current_PC, current_PC);
 			strcpy(BTB_array[current_PC_index].next_PC, next_PC);
 		}
-		strcpy(current_PC, next_PC);  //copy next_PC back into current_PC when finished with comparisons/calcs
+
+		strcpy(current_PC, next_PC);  //copy next_PC back into current_PC when finished with comparison
 	}
 
 }
