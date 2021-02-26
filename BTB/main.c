8d29e3bddb4aea537723d8578c76d745d7864cdf
Josh Johnson <josh.johnson.hire@protonmail.com>
2021-02-26 15:00:55 -0800

    Update main.c
    Added input and output file protections for files not being created

--- BTB/main.c
+++ BTB/main.c
@@ -3,10 +3,22 @@
 int main(void)
 {
 	FILE* input_file;
-	input_file = fopen("full_trace.txt", "r");
+	input_file = fopen(INPUT_FILE, "r");
+
+	if (!input_file)
+	{
+		printf("unable to open input file");
+		return -1;
+	}
 
 	FILE* output_file;
-	output_file = fopen("BTB_array_results.txt", "w+");
+	output_file = fopen(OUTPUT_FILE, "w+");
+
+	if (!output_file)
+	{
+		printf("unable to open output file");
+		return -1;
+	}
 
 	scan_trace(input_file);
 	print_BTB(output_file);
